e22bb033d555aa3fe587edb8222b8f5850bfcd5d
Manuel Antonio Aguilar Padilla <manuel.padilla@unah.hn>
2025-10-20 19:35:54 -0600

    Commit 2, H13: consulta de estado de solicitud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\SolicitudController;
 
 Route::get('/', function () {
     return view('welcome'); // o tu vista principal
@@ -10,9 +11,12 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('plantilla'); // o tu vista principal
 });
+//muestra la vista de buscar estudiante
 Route::get('/estudiantes/buscar', [EstudianteController::class, 'buscar'])->name('estudiantes.buscar');
-
-
+//  muestra la vista de buscar solicitud de matricula
+Route::get('/estado-solicitud', [SolicitudController::class, 'verEstado'])->name('solicitud.verEstado');
+// Ruta para procesar el formulario (POST)
+Route::post('/estado-solicitud', [SolicitudController::class, 'consultarPorDNI']);
 
 
 
